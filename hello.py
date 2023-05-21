8d5c991ab97f994ea22a59729af71dcbaff63ba5
madduri-prasanna <madduriprasanna8@gmail.com>
2023-05-21 12:56:56 +0530

    palindrome program using if else statement

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-# function which return reverse of a string
-  
-def isPalindrome(s):
-    return s == s[::-1]
-  
-  
-# Driver code
-s = "malayalam"
-ans = isPalindrome(s)
-  
-if ans:
-    print("Yes")
-else:
-    print("No")
+Num = int(input("Enter a value:"))  
+Temp = num  
+Rev = 0  
+while(num > 0):  
+    dig = num % 10  
+    revrev = rev * 10 + dig  
+    numnum = num // 10  
+if(temp == rev):  
+    print("This value is a palindrome number!")  
+else:  
+    print("This value is not a palindrome number!")  
+
+
+
 
